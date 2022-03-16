93baff9953fb1071c46efd336dabf4aba9673f41
Anıl Akdemir <36014310+anilakd@users.noreply.github.com>
2022-03-16 19:01:20 +0300

    initialize project

---
+++ src/main/java/com/anilakdemir/softtechfinalproject/SofttechFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.anilakdemir.softtechfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofttechFinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofttechFinalProjectApplication.class, args);
+	}
+
+}
